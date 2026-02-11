6e669678d3f36151e9ad1f01f6817c99ef762d0a
Anastasia <nastyapugach24@gmail.com>
2026-02-11 13:40:51 +0100

    add task solution

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,20 +1,16 @@
-// export const Person = ({ person }) => ();
+/* eslint-disable prettier/prettier */
 export const Person = ({
   person: { name, age, sex, isMarried, partnerName },
-}) => {
-  const title = sex === 'm'
-    ? 'wife'
-    : 'husband';
-
-  return (
-    <section className="Person">
-      <h2 className="Person__name">My name is {name}</h2>
-      {age && <p className="Person__age">I am {age}</p>}
-      <p className="Person__partner">
-        {isMarried
-          ? `${partnerName} is my ${title}`
-          : 'I am not married'}
-      </p>
-    </section>
-  );
-};
+}) => (
+  <section className="Person">
+    <h2 className="Person__name">My name is {name}</h2>
+    {age && <p className="Person__age">I am {age}</p>}
+    <p className="Person__partner">
+      {isMarried
+        ? `${partnerName} is my ${sex === 'm'
+          ? 'wife'
+          : 'husband'}`
+        : 'I am not married'}
+    </p>
+  </section>
+);
